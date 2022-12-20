f84cd5b3603d7cfef7e2ebfa88ebb773e15db74b
fazliddinKhayrullaev11 <fazliddinkhayrullaev4@gmail.com>
2022-12-20 14:27:51 +0500

    this is just little pratise of mine to start with React and Github

---
+++ src/lesson1.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+class December20 extends React.Component{
+
+   
+
+    render(){
+
+
+        return(
+          <div>
+
+            <h1>Hi</h1>
+          </div>
+
+        );
+    }
+
+
+}
+export default December20
+
