0f29044309649a115d6503b4b7df93e9044eb7b6
fazliddinKhayrullaev11 <fazliddinkhayrullaev4@gmail.com>
2022-12-21 01:02:51 +0500

    Nothing here

---
+++ src/Lesson-5/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+class ToDoList extends React.Component{
+  
+    render(){
+
+
+        return(
+            <div>
+                <h1>Hi</h1>
+            </div>
+
+        );
+    }
+
+
+
+
+
+}
+export default ToDoList
